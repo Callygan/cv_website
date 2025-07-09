3d65e57bcf91c14db2bd61468d9f79ff32dbcf59
Callygan <podar.dumitru@bk.ru>
2025-07-09 14:06:11 +0300

    Added the About Me and My Portfolio sections.

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,21 @@
+export default function AboutMe() {
+    return (
+        <section id="AboutMe" className="about--section">
+            <div className="about--section-img">
+                <img src="./img/about-me.png" alt="About Me"/>
+            </div>
+            <div className="hero--section--content--box about--section--box">
+                <div className="hero--section--content">
+                    <p className="section--title">About</p>
+                    <h1 className="skills-section--heading">About Me</h1>
+                    <p className="hero--section-description">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum ullam quae aspernatur dolor, saepe cum voluptatem neque facere consequatur, distinctio commodi repellendus odio vitae ut omnis consectetur, temporibus ipsa molestias.
+                    </p>
+                    <p className="hero--section-description">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum ullam quae aspernatur dolor, saepe cum voluptatem neque facere consequatur.
+                    </p>
+                </div>
+            </div>
+        </section>
+    )
+}
